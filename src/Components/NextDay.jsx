a26150ce4af0d1d6166fd6a39769aa89f3f4b891
xənzheus <91020770+xanzheus@users.noreply.github.com>
2022-04-17 11:06:57 +0700

    Update NextDay.jsx

--- src/Components/NextDay.jsx
+++ src/Components/NextDay.jsx
@@ -26,6 +26,18 @@ function NextDay({ nextDay }) {
           .
         </Text>
       </div>
+ <body class="font-sans dark:bg-gray-700">
+    <section id="bg-particles" class="relative bg-white rounded-b-lg lg:rounded-b-2xl container max-w-5xl mx-auto p-4 h-screen min-h-full dark:bg-gray-800">
+      <div class="relative z-10 h-full">
+        <div class="flex justify-between px-5 pt-5">
+          <a class="toggle-dark mr-5" href="#" onclick="darkMode()">
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-moon-fill" viewBox="0 0 16 16">
+              <path
+                d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z"
+              />
+            </svg>
+          </a>
+
       <div className={styles.buttonDiv}>
         <Button onClick={toggleColorMode}>
           Toggle {colorMode() === "light" ? "dark" : "light"}
