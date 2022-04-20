8cfcc8bed971def326c4b4b36045d11c0451824d
xənzheus <91020770+xanzheus@users.noreply.github.com>
2022-04-21 05:11:51 +0700

    Update NextDay.jsx

--- src/Components/NextDay.jsx
+++ src/Components/NextDay.jsx
@@ -31,6 +31,13 @@ function NextDay({ nextDay }) {
         Toggle {colorMode() === "light" ? "dark" : "light"}
         </Button>
       </div>
+
+<!-- Copyright -->
+    <div class="footer-copyright text-center py-3 text-secondary"><small> Made with
+        <i class="fas fa-heart text-danger animate__animated animate__bounce animate__repeat-3 hati"></i> by
+        <a href="https://www.instagram.com/ryan.frf/"  class="text-decoration-none text-secondary">ryan.frf</a></small>
+    </div>
+
     </HStack>
   );
 }
